d56587afb151b010b15732c4b635814465c7ce61
Wijnand Honselaar <wijnandhonselaar@relatics.com>
2018-01-22 11:36:42 +0100

    Add project files.

---
+++ HelloServiceNet/Service.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace HelloServiceNet
+{
+    public interface IOutput
+    {
+        void Write(string content);
+    }
+
+    public class ConsoleOutput : IOutput
+    {
+        public void Write(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+
+    public interface IDateWriter
+    {
+        void WriteDate();
+    }
+
+    public class TodayWriter : IDateWriter
+    {
+        private readonly IOutput _output;
+
+        public TodayWriter(IOutput output)
+        {
+            _output = output;
+        }
+
+        public void WriteDate()
+        {
+            _output.Write(DateTime.Today.ToShortDateString());
+        }
+    }
+
+    /**
+     * Message interface used for events
+     */
+    public interface IMessage
+    {
+        string Name { get; set; }
+        string Message { get; set; }
+    }
+
+    /**
+     * Listener to process events from the Event Queue
+     */
+    public static class Listener
+    {
+        public static string Name = "";
+
+        /**
+         * Consume message from the Event Queue
+         */
+        public static async Task Consume(ConsumeContext<IMessage> ctx)
+        {
+            if (ShowMessage(ctx.Message))
+                await Console.Out.WriteLineAsync(ctx.Message.Name + " - " + ctx.Message.Message);
+        }
+
+        /**
+         * Filter own messages
+         */
+        private static bool ShowMessage(IMessage m)
+        {
+            return Name != m.Name;
+        }
+    }
+}
---
+++ MassTransitHelloWorld/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Autofac;
+using HelloServiceNet;
+using MassTransit;
+using IContainer = Autofac.IContainer;
+
+namespace MessageQueueMessenger
+{
+    class Program
+    {
+        private static IContainer Container { get; set; }
+        static void Main(string[] args)
+        {
+            // Using Autofac to autoinject classes
+            var builder = new ContainerBuilder();
+            // Register interface for autoinjection when ConsoleOuput is instantiated.
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            // Register interface for autoinjection when TodayWriter is instantiated.
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+            // Build container to create the register
+            Container = builder.Build();
+            // Execute function that uses the Autoinjected classes
+            WriteDate();
+            
+            //
+            Console.WriteLine("Enter your name");
+            var name = Console.ReadLine();
+            Console.WriteLine("Connecting to message bus...");
+
+            var busControl = ConfigureBus(name);
+            busControl.Start();
+
+            do
+            {
+                Console.WriteLine("Enter message (or quit to exit)");
+                Console.Write("> ");
+                string value = Console.ReadLine();
+
+                if ("quit".Equals(value, StringComparison.OrdinalIgnoreCase))
+                {
+                    busControl.Publish<IMessage>(new
+                    {
+                        Name = name,
+                        Message = "<< Disconnected >>"
+                    });
+                
+                    break;
+                }
+
+                busControl.Publish<IMessage>(new
+                {
+                    Name = name,
+                    Message = value
+                });
+            }
+            while (true);
+
+            busControl.StopAsync(TimeSpan.FromMilliseconds(5000));
+        }
+
+        /**
+         *  Write current date to the console.
+         */
+        public static void WriteDate()
+        {
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+            }
+        }
+
+        /**
+         * Configure RabbitMQ Event Queue
+         */
+        private static IBusControl ConfigureBus(string name)
+        {
+            return Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                // Set sender name, to filter own messages.
+                Listener.Name = name;
+                // Configure connection to RabbitMQ host
+                var host = cfg.Host(new Uri("rabbitmq://192.168.10.193"), h =>
+                {
+                    h.Username("rabbitmq");
+                    h.Password("rabbitmq");
+                });
+                // Setup listener
+                cfg.ReceiveEndpoint(host, "event_queue_" + name, e =>
+                {
+                    // Call the Event Consumer
+                    e.Handler<IMessage>(Listener.Consume);
+                });
+            });
+        }
+    }
+}
